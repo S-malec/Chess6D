c2844c5b210294a44726bf1e3154fa3078fc6e68
SmalecWalec <barteks161@gmail.com>
2026-04-08 18:33:36 +0200

    Refactor Checkerboard

--- src/main/java/org/example/Checkerboard.java
+++ src/main/java/org/example/Checkerboard.java
@@ -10,24 +10,24 @@ public class Checkerboard {
 
     public Checkerboard(int N) {
         this.N = N;
-        szachownica = new char[N+2][N+2];
+        szachownica = new char[N + 2][N + 2];
         prepare();
     }
 
     public void prepare() {
-        N+=2;
+        N += 2;
         szachownica[0][0] = '@';
-        szachownica[0][N-1] = '@';
-        szachownica[N-1][0] = '@';
-        szachownica[N-1][N-1] = '@';
+        szachownica[0][N - 1] = '@';
+        szachownica[N - 1][0] = '@';
+        szachownica[N - 1][N - 1] = '@';
         for (int i = 0; i < N; i++) {
             szachownica[0][i] = '@';
             szachownica[i][0] = '@';
-            szachownica[N-1][i] = '@';
-            szachownica[i][N-1] = '@';
+            szachownica[N - 1][i] = '@';
+            szachownica[i][N - 1] = '@';
         }
-        for (int i = 1; i < N-1; i++) {
-            for (int j = 1; j < N-1; j++) {
+        for (int i = 1; i < N - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
                 szachownica[i][j] = '0';
             }
         }
@@ -40,40 +40,42 @@ public class Checkerboard {
                 sz.append(szachownica[i][j]);
                 sz.append(" ");
             }
-            sz.append( "\n" );
+            sz.append("\n");
         }
         return sz.toString();
     }
 
     public void placeK(int i, int j) {
-        if (i>=N-2 || j>=N-2) throw new IndexOutOfBoundsException();
-        i+=1;
-        j+=1;
+        if (i >= N - 2 || j >= N - 2)
+            throw new IndexOutOfBoundsException();
+        i += 1;
+        j += 1;
         szachownica[i][j] = 'K';
         Pionek p = new Pionek(i, j);
         pionki.add(p);
     }
 
     public void placeStar(int i, int j) {
-        i+=1;
-        j+=1;
+        i += 1;
+        j += 1;
         szachownica[i][j] = '*';
     }
 
     public void placeCannon(int i, int j, char c) {
-        if (i>=N-2 || j>=N-2) throw new IndexOutOfBoundsException();
-        i+=1;
-        j+=1;
+        if (i >= N - 2 || j >= N - 2)
+            throw new IndexOutOfBoundsException();
+        i += 1;
+        j += 1;
         szachownica[i][j] = c;
-        Cannon armata = new Cannon(i, j , c);
+        Cannon armata = new Cannon(i, j, c);
         cannons.add(armata);
     }
 
     public void attack(Pionek p) {
-        updateCell(p.i, p.j - 1);     // lewo
-        updateCell(p.i, p.j + 1);     // prawo
-        updateCell(p.i - 1, p.j);     // gora
-        updateCell(p.i + 1, p.j);     // dol
+        updateCell(p.i, p.j - 1); // lewo
+        updateCell(p.i, p.j + 1); // prawo
+        updateCell(p.i - 1, p.j); // gora
+        updateCell(p.i + 1, p.j); // dol
 
         updateCell(p.i + 1, p.j + 1); // skos dol-prawo
         updateCell(p.i - 1, p.j - 1); // skos gora-lewo
@@ -83,62 +85,62 @@ public class Checkerboard {
 
     public void funnyAttack(Pionek p) {
         // pionek po lewej
-        if (String.valueOf(szachownica[p.i][p.j-1]).equals("@")) {
-            updateCell(p.i-1, N-2);
-            updateCell(p.i, N-2);
-            updateCell(p.i+1, N-2);
+        if (szachownica[p.i][p.j - 1] == '@') {
+            updateCell(p.i - 1, N - 2);
+            updateCell(p.i, N - 2);
+            updateCell(p.i + 1, N - 2);
         }
         // pionek po prawej
-        if (String.valueOf(szachownica[p.i][p.j+1]).equals("@")) {
-            updateCell(p.i-1, 1);
+        if (szachownica[p.i][p.j + 1] == '@') {
+            updateCell(p.i - 1, 1);
             updateCell(p.i, 1);
-            updateCell(p.i+1, 1);
+            updateCell(p.i + 1, 1);
         }
         // pionek na gorze
-        if (String.valueOf(szachownica[p.i-1][p.j]).equals("@")) {
-            updateCell(N-2, p.j-1);
-            updateCell(N-2, p.j);
-            updateCell(N-2, p.j+1);
+        if (szachownica[p.i - 1][p.j] == '@') {
+            updateCell(N - 2, p.j - 1);
+            updateCell(N - 2, p.j);
+            updateCell(N - 2, p.j + 1);
         }
         // pionek na dole
-        if (String.valueOf(szachownica[p.i+1][p.j]).equals("@")) {
-            updateCell(1, p.j-1);
+        if (szachownica[p.i + 1][p.j] == '@') {
+            updateCell(1, p.j - 1);
             updateCell(1, p.j);
-            updateCell(1, p.j+1);
+            updateCell(1, p.j + 1);
         }
     }
 
     private void cannonAttack(Cannon c) {
         // pionek po lewej
-        if (String.valueOf(szachownica[c.i][c.j-1]).equals("K")) {
+        if (szachownica[c.i][c.j - 1] == 'K') {
             cannonShot(c);
         }
         // pionek po prawej
-        if (String.valueOf(szachownica[c.i][c.j+1]).equals("K")) {
+        if (szachownica[c.i][c.j + 1] == 'K') {
             cannonShot(c);
         }
         // pionek na gorze
-        if (String.valueOf(szachownica[c.i-1][c.j]).equals("K")) {
+        if (szachownica[c.i - 1][c.j] == 'K') {
             cannonShot(c);
         }
         // pionek na dole
-        if (String.valueOf(szachownica[c.i+1][c.j]).equals("K")) {
+        if (szachownica[c.i + 1][c.j] == 'K') {
             cannonShot(c);
         }
         // pionek lewo gora
-        if (String.valueOf(szachownica[c.i-1][c.j+1]).equals("K")) {
+        if (szachownica[c.i - 1][c.j + 1] == 'K') {
             cannonShot(c);
         }
         // pionek prawo gora
-        if (String.valueOf(szachownica[c.i+1][c.j+1]).equals("K")) {
+        if (szachownica[c.i + 1][c.j + 1] == 'K') {
             cannonShot(c);
         }
         // pionek lewo dol
-        if (String.valueOf(szachownica[c.i-1][c.j-1]).equals("K")) {
+        if (szachownica[c.i - 1][c.j - 1] == 'K') {
             cannonShot(c);
         }
         // pionek prawo dol
-        if (String.valueOf(szachownica[c.i+1][c.j-1]).equals("K")) {
+        if (szachownica[c.i + 1][c.j - 1] == 'K') {
             cannonShot(c);
         }
     }
@@ -156,48 +158,38 @@ public class Checkerboard {
         return !Character.isDigit(szachownica[row][col]);
     }
 
-    private int isCanon(int row, int col) {
-        return switch (szachownica[row][col]) {
-            case '/' -> 1;
-            case '|' -> 2;
-            case '\\' -> 3;
-            case '-' -> 4;
-            default -> 0;
-        };
-    }
-
-    private void cannonShot(Cannon c){
+    private void cannonShot(Cannon c) {
         switch (c.c) {
             case '|':
-                for (int i = c.i; i >= 0; i--) {
+                for (int i = 0; i < szachownica.length; i++) {
                     updateCell(i, c.j);
                 }
-                for (int i = c.i; i < N; i++) {
-                    updateCell(i, c.j);
-                }
+                break;
             case '-':
-                for (int j = c.j; j >= 0; j--) {
+                for (int j = 0; j < szachownica[0].length; j++) {
                     updateCell(c.i, j);
                 }
-                for (int j = c.j; j < N; j++) {
-                    updateCell(c.i, j);
-                }
+                break;
             case '\\':
-                for (int j = c.j; j >= 0; j--) {
-                    for (int i = c.i; i >= 0; i--) {
-                        if (c.i - i == c.j - j) {
-                            updateCell(i, j);
-                        }
-                    }
+                // Lewo-Góra (i--, j--)
+                for (int i = c.i, j = c.j; i >= 0 && j >= 0; i--, j--) {
+                    updateCell(i, j);
                 }
-                for (int j = c.j; j < N; j++) {
-                    for (int i = c.i; i < N; i++) {
-                        if (c.i - i == c.j - j) {
-                            updateCell(i, j);
-                        }
-                    }
+                // Prawo-Dół (i++, j++)
+                for (int i = c.i, j = c.j; i < N && j < N; i++, j++) {
+                    updateCell(i, j);
                 }
-
+                break;
+            case '/':
+                // Prawo-Góra (i++, j--)
+                for (int i = c.i, j = c.j; i < N && j >= 0; i++, j--) {
+                    updateCell(i, j);
+                }
+                // Lewo-Dół (i--, j++)
+                for (int i = c.i, j = c.j; i >= 0 && j < N; i--, j++) {
+                    updateCell(i, j);
+                }
+                break;
         }
     }
 
@@ -206,6 +198,5 @@ public class Checkerboard {
         pionki.forEach(this::funnyAttack);
         cannons.forEach(this::cannonAttack);
     }
-
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Checkerboard sz = new Checkerboard(5);
 
-//        sz.placeK(0,1);
-//        sz.placeK(0,2);
-//        sz.placeStar(0,3);s
-//        sz.calcAttack();
-        sz.placeK(2,2);
-        sz.placeCannon(3,2,'\\');
+        // sz.placeK(0,1);
+        // sz.placeK(0,2);
+        // sz.placeStar(0,3);
+        // sz.calcAttack();
+        sz.placeK(4, 0);
+        // sz.placeCannon(3, 2, '|');
         sz.calcAttack();
         System.out.println(sz.display());
 
